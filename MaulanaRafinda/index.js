6481148fb7d27f1b0f5fb0ba87bc6275223c524f
Maulana <maulana.rafinda.tif20@gmail.com>
2023-09-10 14:36:15 +0700

    First Commit Challange

---
+++ MaulanaRafinda/index.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+
+$nama = "Maulana Rafinda";
+console.log("Halo nama saya adalah", $nama);
+
+let nilai1 = 9;
+let nilai2 = 10;
+let total = nilai1 + nilai2;
+console.log(total, "adalah hasil penambahan nilai 1 dan nilai 2");
+
+nilai1 = "9";
+nilai2 = "10"; // Kongkatinet adalah penggabungan string.
+hasil = nilai1 + nilai2;
+console.log(hasil);
+
